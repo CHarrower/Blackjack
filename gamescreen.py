c43dabe0907dc603813b5f988eff823340d06b48
Craig <craigharrower02@hotmail.com>
2024-06-03 23:38:57 +0100

    comments added

--- gamescreen.py
+++ gamescreen.py
@@ -30,13 +30,21 @@ class GameScreen:
         self.show_cards()
         self.create_hit_stand_buttons()
 
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # LOAD CARD IMAGES
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def load_card_images(self, directory, extension):
         card_images = []
         for filename in os.listdir(directory):
             if filename.endswith(extension):
                  card_images.append(os.path.join(directory, filename))
         return card_images
+    #------------------------------------------------------------------------------------------------------------->
+    
 
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # SHOW RANDOM CARD
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def show_random_card(self, frame, faceup=True):
         if faceup:
             random_card_path = random.choice(self.card_images)
@@ -49,7 +57,12 @@ class GameScreen:
         card_label.photo = card_photo
         card_label.pack(side=tk.LEFT)
         return card_label
+    #------------------------------------------------------------------------------------------------------------->
 
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # SHOW CARDS
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def show_cards(self):
         # Show dealer's cards
         self.show_random_card(self.dealer_frame, faceup=False)  
@@ -58,27 +71,39 @@ class GameScreen:
         # Show player's cards
         self.show_random_card(self.player_frame, faceup=False)  
         self.show_random_card(self.player_frame, faceup=True)  
-        
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # CREATE HIT AND STAND BUTTONS
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def create_hit_stand_buttons(self):
-    # Image paths for button states
+        # Image paths for button states
         hit_image_paths = ["pix/button/Button_White (8).png", "pix/button/Button_White (1).png"]
         stand_image_paths = ["pix/button/Button_Dark (8).png", "pix/button/Button_Dark (1).png"]
 
-    # Check if the image paths are correct and files exist
+        # Check if the image paths are correct and files exist
         for path in hit_image_paths + stand_image_paths:
             if not os.path.exists(path):
                 print(f"Image not found: {path}")
 
+        # Button coordinates
         h_button_x, h_button_y = 544, 500
         s_button_x, s_button_y = 645, 500
 
+        # Text coordinates
         h_text_x, h_text_y = 553, 560
         s_text_x, s_text_y = 631, 560
 
-    # Create button animations
+        # Create button animations
         self.create_button_animation(hit_image_paths, h_button_x, h_button_y, self.hit_action, h_text_x, h_text_y, "Hit")
         self.create_button_animation(stand_image_paths, s_button_x, s_button_y, self.stand_action, s_text_x, s_text_y, "Stand")
+    #------------------------------------------------------------------------------------------------------------->
 
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # CREATE BUTTON ANIMATION
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def create_button_animation(self, image_paths, x, y, action, text_x, text_y, text):
         images = [Image.open(path).convert("RGBA").resize((60, 60)) for path in image_paths]
         photo_list = [ImageTk.PhotoImage(image) for image in images]
@@ -102,15 +127,32 @@ class GameScreen:
         label = tk.Label(master=self.frame, text=text, bg="green", font=("Attomic", 45))
         label.place(x=text_x, y=text_y)
     
+    #------------------------------------------------------------------------------------------------------------->
+
+    
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # HIT ACTION
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def hit_action(self, event):
         # Print statement for debugging
         print("Hit button pressed")
+    #------------------------------------------------------------------------------------------------------------->
 
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # STAND ACTION
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def stand_action(self, event):
         # Print statement for debugging
         print("Stand button pressed")
+    #------------------------------------------------------------------------------------------------------------->
 
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# MAIN FUNCTION
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if __name__ == "__main__":
     win = tk.Tk()
     app = GameScreen(win)
     win.mainloop()
+#------------------------------------------------------------------------------------------------------------->
