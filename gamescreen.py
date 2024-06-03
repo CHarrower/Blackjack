91a3716a38beb9a60fbfc59d4d01d0874d6221b9
Craig <craigharrower02@hotmail.com>
2024-06-03 21:06:52 +0100

    dealers starting card now showing -

--- gamescreen.py
+++ gamescreen.py
@@ -1,8 +1,7 @@
 import tkinter as tk 
-from tkinter import font as tkFont 
 from PIL import Image, ImageTk
 import random
-
+import os
 
 class GameScreen:
     def __init__(self, master, width=750, height=750):
@@ -10,3 +9,44 @@ class GameScreen:
         self.master.geometry(f"{width}x{height}")
         self.frame = tk.Frame(master=self.master, width=width, height=height, bg="green")
         self.frame.pack()
+
+        self.card_images = self.load_card_images("main_cards", ".png")
+
+        self.faceup_card_label = tk.Label(master=self.frame, bg="green", bd=0)
+        self.faceup_card_label.place(x=100, y=50)
+
+        self.back_of_card_p = "main_cards/back_4.png"
+        self.back_of_card_img = Image.open(self.back_of_card_p).convert("RGBA").resize((45, 56))
+        self.back_of_card_phot = ImageTk.PhotoImage(self.back_of_card_img)
+
+        self.back_of_card_label = tk.Label(master=self.frame, bg="green", bd=0)
+        self.back_of_card_label.place(x=50, y=50)
+
+        self.show_cards()
+
+    def load_card_images(self, directory, extension):
+        card_images = []
+        for filename in os.listdir(directory):
+            if filename.endswith(extension):
+                 card_images.append(os.path.join(directory, filename))
+        return card_images
+
+    def show_random_faceup(self):
+        random_card_path = random.choice(self.card_images)
+        card_image = Image.open(random_card_path).convert("RGBA").resize((45, 56))
+        card_photo = ImageTk.PhotoImage(card_image)
+        self.faceup_card_label.config(image=card_photo)
+        self.faceup_card_label.image = card_photo
+
+    def show_back_of_card(self):
+        self.back_of_card_label.config(image=self.back_of_card_phot)
+        self.back_of_card_label.image = self.back_of_card_phot
+
+    def show_cards(self):
+        self.show_random_faceup()
+        self.show_back_of_card()
+
+if __name__ == "__main__":
+    win = tk.Tk()
+    app = GameScreen(win)
+    win.mainloop()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from titlescreen import TitleScreen
+from gamescreen import GameScreen
 import tkinter as tk 
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -7,8 +8,20 @@ import tkinter as tk
 
 def main():
     win = tk.Tk()
-    app = TitleScreen(win, title="Blackjack")
+    title_app = TitleScreen(win, title="Blackjack")
+    title_app.on_play_button_clicked = lambda: show_game_screen(win)
     win.mainloop()
+
+def show_game_screen(master):
+    master.withdraw()  # Hide the title screen window
+    game_win = tk.Toplevel(master)
+    game_screen = GameScreen(game_win)
+    game_screen.show_random_faceup()  # Show one card facing up
+    game_win.protocol("WM_DELETE_WINDOW", lambda: close_game_screen(master, game_win))
+
+def close_game_screen(master, game_win):
+    game_win.destroy()
+    master.deiconify()  # Show the title screen window again
 
 if __name__ == "__main__":
     main()
