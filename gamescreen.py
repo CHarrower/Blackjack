504e49d03bf0c1932e9e5a374777b328d46db9fc
Craig <craigharrower02@hotmail.com>
2024-06-07 03:55:06 +0100

    comments

--- gamescreen.py
+++ gamescreen.py
@@ -3,8 +3,12 @@ from PIL import Image, ImageTk
 import random
 import os
 
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Game Screen Class
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class GameScreen:
     def __init__(self, master, width=750, height=750):
+        # Initialize game screen
         self.master = master
         self.width = width
         self.height = height
@@ -12,28 +16,26 @@ class GameScreen:
         self.frame = tk.Frame(master=self.master, width=width, height=height, bg="green")
         self.frame.pack()
 
+        # Load card images and values
         self.card_images = self.load_card_images("main_cards", ".png")
         self.card_values = self.load_card_values()
 
+        # Create frames for dealer and player
         self.dealer_frame = tk.Frame(master=self.frame, bg="green")
         self.dealer_frame.place(x=50, y=50)
-
         self.player_frame = tk.Frame(master=self.frame, bg="green")
         self.player_frame.place(x=50, y=605)
 
+        # Initialize variables
         self.dealer_cards = []
         self.player_cards = []
-
         self.back_of_card_p = "main_cards/back/back_4.png"
         self.back_of_card_img = Image.open(self.back_of_card_p).convert("RGBA").resize((55, 66))
         self.back_of_card_phot = ImageTk.PhotoImage(self.back_of_card_img)
-
         self.dealer_total_label = tk.Label(master=self.frame, text="Dealer: 0", bg="green", font=("Attomic", 40))
         self.dealer_total_label.place(x=50, y=115)
-
         self.player_total_label = tk.Label(master=self.frame, text="Player: 0", bg="green", font=("Attomic", 40))
         self.player_total_label.place(x=50, y=555)
-
         self.play_again_button = None
         self.winner_label = None
         self.current_image_index = [0]
@@ -41,23 +43,30 @@ class GameScreen:
             ImageTk.PhotoImage(Image.open("pix/button/Button_White (6).png").resize((200, 50))),
             ImageTk.PhotoImage(Image.open("pix/button/Button_White (7).png").resize((200, 50)))
         ]
-
         self.player_total = 0
         self.dealer_total = 0
         self.show_cards()
         self.hit_button = None
         self.stand_button = None
         self.create_hit_stand_buttons()
+#------------------------------------------------------------------------------------------------------------->
 
-    # LOAD CARD IMAGES
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Load card images
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def load_card_images(self, directory, extension):
         card_images = []
         for filename in os.listdir(directory):
             if filename.endswith(extension):
                  card_images.append(os.path.join(directory, filename))
         return card_images
-    
-    # LOAD CARD VALUES
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Load card values
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def load_card_values(self):
         card_values = {}
         for filename in os.listdir("main_cards"):
@@ -66,8 +75,12 @@ class GameScreen:
                 value = self.get_card_value(card_name)
                 card_values[os.path.join("main_cards", filename)] = value
         return card_values
-    
-    # GET CARD VALUE
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Get card value
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def get_card_value(self, card_name):
         parts = card_name.split("_")
         if len(parts) != 2:
@@ -81,8 +94,12 @@ class GameScreen:
             return 11
         else:
             return int(value_str)
-    
-    # SHOW RANDOM CARD
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Show random card
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def show_random_card(self, frame, faceup=True):
         if faceup:
             random_card_path = random.choice(self.card_images)
@@ -97,8 +114,12 @@ class GameScreen:
         card_label.photo = card_photo
         card_label.pack(side=tk.LEFT)
         return card_label, card_value
-    
-    # SHOW CARDS
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Show cards
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def show_cards(self):
         # Show dealer's cards
         self.show_random_card(self.dealer_frame, faceup=False)
@@ -111,14 +132,18 @@ class GameScreen:
         _, value = self.show_random_card(self.player_frame, faceup=True)
         self.player_total += value
         self.update_totals()
-    
-    # CREATE HIT AND STAND BUTTONS
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Create hit and stand buttons
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def create_hit_stand_buttons(self):
         # Image paths for button states
         hit_image_paths = ["pix/button/Button_White (8).png", "pix/button/Button_White (1).png"]
         stand_image_paths = ["pix/button/Button_Dark (8).png", "pix/button/Button_Dark (1).png"]
 
-        # Check if the image paths are correct and files exist
+        # Check if the image paths are correct
         for path in hit_image_paths + stand_image_paths:
             if not os.path.exists(path):
                 print(f"Image not found: {path}")
@@ -134,8 +159,12 @@ class GameScreen:
         # Create button animations
         self.hit_button = self.create_button_animation(hit_image_paths, h_button_x, h_button_y, self.hit_action, h_text_x, h_text_y, "Hit")
         self.stand_button = self.create_button_animation(stand_image_paths, s_button_x, s_button_y, self.stand_action, s_text_x, s_text_y, "Stand")
-    
-    # CREATE BUTTON ANIMATION
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Create button animation
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def create_button_animation(self, image_paths, x, y, action, text_x, text_y, text):
         images = [Image.open(path).convert("RGBA").resize((60, 60)) for path in image_paths]
         photo_list = [ImageTk.PhotoImage(image) for image in images]
@@ -160,25 +189,42 @@ class GameScreen:
         label.place(x=text_x, y=text_y)
         
         return button  # Return the button widget
-# UPDATE TOTALS
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Update totals
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def update_totals(self):
         self.player_total_label.config(text=f"Player: {self.player_total}")
         self.dealer_total_label.config(text=f"Dealer: {self.dealer_total}")
         self.check_game_over()
-    
-    # HIT ACTION
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Hit action
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def hit_action(self, event):
         print("Hit button pressed")
         _, value = self.show_random_card(self.player_frame, faceup=True)
         self.player_total += value
         self.update_totals()
-    
-    # STAND ACTION
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Stand action
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def stand_action(self, event):
         print("Stand button pressed")
         self.play_dealer_turn()
-    
-    # DEALER TURN
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Dealer turn
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def play_dealer_turn(self):
         if self.dealer_total < 17:
             _, value = self.show_random_card(self.dealer_frame, faceup=True)
@@ -187,12 +233,16 @@ class GameScreen:
             self.master.after(1000, self.play_dealer_turn)  # Delay of 1 second between dealer hits
         else:
             self.check_game_over(player_standing=True)
-    
-    # CHECK GAME OVER
+    #------------------------------------------------------------------------------------------------------------->
+
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Check game over
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def check_game_over(self, player_standing=False):
         if self.player_total == 21:
-            print("Player wins with 21!")
-            self.show_win_screen("Player wins with 21!")
+            print("Player wins!")
+            self.show_win_screen("Player wins!")
         elif self.player_total > 21:
             print("Player busts!")
             self.show_win_screen("Dealer wins!")
@@ -209,13 +259,16 @@ class GameScreen:
             else:
                 print("It's a tie!")
                 self.show_win_screen("It's a tie!")
+    #------------------------------------------------------------------------------------------------------------->
 
-    # SHOW WIN SCREEN
+
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Show win screen
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def show_win_screen(self, winner_text):
         # Hide hit and stand buttons
         self.hit_button.place_forget()
         self.stand_button.place_forget()
-        
 
         # Display the winner text
         winner_label = tk.Label(master=self.frame, text=winner_text, bg="green", font=("Attomic", 45))
@@ -243,9 +296,12 @@ class GameScreen:
         play_again_button.place(x=540, y=350)
         play_again_button.bind("<Button-1>", play_again_clicked)
         self.play_again_button = play_again_button
+    #------------------------------------------------------------------------------------------------------------->
 
 
-    # RESET GAME
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # Reset game
+    #/////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def reset_game(self):
         print("Resetting game...")
         # Clear dealer and player cards from the screen
@@ -274,8 +330,12 @@ class GameScreen:
         
         # Show new cards for the dealer and the player
         self.show_cards()
+    #------------------------------------------------------------------------------------------------------------->
 
-# MAIN FUNCTION
+    
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# Main function
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if __name__ == "__main__":
     win = tk.Tk()
     app = GameScreen(win)
